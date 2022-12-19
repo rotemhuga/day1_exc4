4b586860c3699d8916ab1d87130aa7e329ee12d4
rotem huga <rotem.3007@gmail.com>
2022-12-19 18:18:35 +0200

    functionsjs

---
+++ main.js
@@ -0,0 +1,9 @@
+function handleClick () {
+    console.log('Element Clicked');
+    PlacesILove.style.color = "blue";
+    contactMe.style.color = "blue";
+}
+
+function delayHandleClick () {
+setTimeout(handleClick ,3000)
+}
