44b7cb119f482674adb2b3c66aa30a40ae3591cd
rotem huga <rotem.3007@gmail.com>
2022-12-21 22:24:18 +0200

    darkmode

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Option1
 function handleClick () {
     console.log('Element Clicked');
     PlacesILove.style.color = "blue";
@@ -6,4 +7,25 @@ function handleClick () {
 
 function delayHandleClick () {
 setTimeout(handleClick ,3000)
+}
+
+// //option2
+// function handleClick (id) {
+//     console.log('Element Clicked');
+//     document.getElementById(id).style.color="green";
+// }
+
+// function delayHandleClick (id) {
+//     setTimeout(()=> handleClick(id), 3000)
+//     }
+
+function Dark(){
+    let flag = document.getElementById(`Dark-Mode`).value;
+    if (flag == `off`){
+        document.body.setAttribute(`class`, `Dark-css`)
+        document.getElementById(`Dark-Mode`).value = `on`
+    } else{
+        document.body.setAttribute(`class`, `body`)
+        document.getElementById(`Dark-Mode`).value = `off`
+    }
 }
