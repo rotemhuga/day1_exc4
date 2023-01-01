112cd71940a5bef68e010073f10e0725155a1607
rotem huga <rotem.3007@gmail.com>
2023-01-01 23:21:20 +0200

    new images

--- main.js
+++ main.js
@@ -39,3 +39,37 @@ for (let index = 0; index < favoriteFood.length; index++) {
 makeList += `</ul>`;
 console.log(makeList);
 document.getElementById("my-id").outerHTML = makeList;
+
+const imageArr = [
+{   name:"Palacio Real de Madrid",
+    img:"assets/image1_placio.jpg",
+    description:"is the official residence of the Spanish royal family at the city of Madrid",
+    built: "April 7, 1735"
+},
+{
+    name: "Eiffel Tower",
+    img: "assets/image2_eiffel-tower.jpg",
+    description: "is a wrought-iron lattice tower on the Champ de Mars in Paris, France",
+    built: "31 March 1889"
+},
+{
+    name: "Statue of Liberty",
+    img: "assets/image_3_state-of-liberty.jpg",
+    description: "was a gift from France as a symbol of American freedom",
+    built: "October 28, 1886"
+}
+]
+
+function renderImegList(items) {
+    let makeList = ``;
+    // //   makeList += `<div>`
+    for (let i = 0; i < items.length; i++) {
+        makeList += `<img src='${items[i].img}'><p>${items[i].name},
+                    ${items[i].description},
+                    ${items[i].built}</p></br>`;
+    // //   makeList += `</div>`
+    }
+    console.log(makeList);
+     return makeList;
+  }
+  document.getElementById('images_description').innerHTML = renderImegList(imageArr);
